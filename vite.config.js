59087956223f109c6e51b21fcfe97c59a243e874
Sue Raisianzadeh <raisianz@gmail.com>
2024-03-02 19:16:56 +1300

    homepage

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-import reactRefresh from '@vitejs/plugin-react-refresh'
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-refresh'
 
 export default defineConfig({
-  plugins: [reactRefresh()],
+  plugins: [react()],
   server: {
     proxy: {
       '/api': {
