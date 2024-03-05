44dd39784f48a579ee140c6aab27d63c0fd3d5ad
Sue Raisianzadeh <raisianz@gmail.com>
2024-03-05 18:14:24 +1300

    edit

--- server/database/knexfile.js
+++ server/database/knexfile.js
@@ -1,14 +1,23 @@
 const path = require('path')
-
 require('dotenv').config({ path: path.join(__dirname, '../../.env') })
 
 module.exports = {
   development: {
-    client: 'sqlite3',
+    client: 'mysql',
     connection: {
-      filename: path.join(__dirname, '../../database/dev.sqlite3'),
+      host: 'localhost',
+      user: 'your_username',
+      password: 'your_password',
+      database: 'your_database_name',
+      charset: 'utf8',
     },
-    useNullAsDefault: true,
+    migrations: {
+      tableName: 'knex_migrations',
+      directory: path.join(__dirname, '/migrations'),
+    },
+    seeds: {
+      directory: path.join(__dirname, '/seeds'),
+    },
   },
 
   production: {
